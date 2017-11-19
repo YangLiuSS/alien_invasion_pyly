eab53f518692c60c6b618027ac800ffde1c6a865
ly <408855838@qq.com>
2017-11-19 22:55:30 +0800

    game start

---
+++ alien_invasion.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# @Author  : ly
+import sys
+
+import pygame
+
+
+def run_game():
+    pygame.init()
+    screen = pygame.display.set_mode((1200, 800))
+    pygame.display.set_caption('Alien Invasion')
+
+    while True:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        pygame.dispaly.flip()
+
+
+run_game()
+
